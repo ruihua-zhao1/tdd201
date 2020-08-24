4849c7614a15fdd3434daa469fe308af6a84e01b
Ruihua Zhao <ruihua.zhao@myob.com>
2020-08-24 22:21:25 +0800

    rename test cases

--- tdd201/src/test/java/PerformanceTicketsTest.java
+++ tdd201/src/test/java/PerformanceTicketsTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 public class PerformanceTicketsTest {
     @Test
-    public void verifySpecialTicketProduct() {
+    public void verifyRise1Before14Days() {
         PerformanceTickets performanceTickets = new PerformanceTickets(15, 20);
         int actualQuality = performanceTickets.updatedQuality(14);
         Assert.assertEquals(21, actualQuality);
