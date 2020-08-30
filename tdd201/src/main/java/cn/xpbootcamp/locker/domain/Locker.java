317afad97b642de3a51c1be5be6523c8cee8e48a
jkxue <jkxupt@163.com>
2020-08-30 08:58:42 +0800

    cover test case 'given_lockerA_has_zero_available_space_and_bagA_want_to_be_saved_when_bagA_is_storing_then_store_failed_and_get_error_message'

--- tdd201/src/main/java/cn/xpbootcamp/locker/domain/Locker.java
+++ tdd201/src/main/java/cn/xpbootcamp/locker/domain/Locker.java
@@ -1,10 +1,23 @@
 package cn.xpbootcamp.locker.domain;
 
+import cn.xpbootcamp.locker.exception.NoAvailableSpaceException;
+
 public class Locker {
+    private Integer availableSpaceNumber;
+
     public Ticket store(Bag bagA) {
-        return new Ticket();
+        if(this.getAvailableSpaceNumber() == 0){
+            throw new NoAvailableSpaceException("No available space");
+        }else {
+            return new Ticket();
+        }
     }
 
-    public void setAvailableSpaceNumber(int i) {
+    public Integer getAvailableSpaceNumber() {
+        return availableSpaceNumber;
+    }
+
+    public void setAvailableSpaceNumber(Integer availableSpaceNumber) {
+        this.availableSpaceNumber = availableSpaceNumber;
     }
 }
