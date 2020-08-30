5e3665ab8aad37d835ed2ba46b4a76213227aa2c
jkxue <jkxupt@163.com>
2020-08-30 09:31:40 +0800

    cover test case 'given lockerA stored a bagA and ticketA of bagA when get bagA with ticketA then get bagA'

--- tdd201/src/main/java/cn/xpbootcamp/locker/domain/Locker.java
+++ tdd201/src/main/java/cn/xpbootcamp/locker/domain/Locker.java
@@ -2,14 +2,19 @@ package cn.xpbootcamp.locker.domain;
 
 import cn.xpbootcamp.locker.exception.NoAvailableSpaceException;
 
+import java.util.HashMap;
+
 public class Locker {
     private Integer availableSpaceNumber;
+    private HashMap<Ticket, Bag> bagMap = new HashMap<>();
 
-    public Ticket store(Bag bagA) {
+    public Ticket store(Bag bag) {
         if(this.getAvailableSpaceNumber() == 0){
             throw new NoAvailableSpaceException("No available space");
         }else {
-            return new Ticket();
+            Ticket ticket = new Ticket();
+            bagMap.put(ticket, bag);
+            return ticket;
         }
     }
 
@@ -21,7 +26,7 @@ public class Locker {
         this.availableSpaceNumber = availableSpaceNumber;
     }
 
-    public Bag getBag(Ticket ticketA) {
-        return null;
+    public Bag getBag(Ticket ticket) {
+        return bagMap.get(ticket);
     }
 }
