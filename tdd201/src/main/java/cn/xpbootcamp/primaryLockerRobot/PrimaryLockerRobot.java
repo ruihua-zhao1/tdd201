9abe28cdcadba24b46e54899808d10d6e24781d6
Ruihua Zhao <ruihua.zhao@myob.com>
2020-09-01 23:30:59 +0800

    add given_PrimaryLockerRobot_manages_LockerA_and_lockerA_has_one_available_space_when_PrimaryLockerRobot_stores_bagA_then_store_successfully_and_get_valid_ticketA

--- tdd201/src/main/java/cn/xpbootcamp/primaryLockerRobot/PrimaryLockerRobot.java
+++ tdd201/src/main/java/cn/xpbootcamp/primaryLockerRobot/PrimaryLockerRobot.java
@@ -1,10 +1,18 @@
 package cn.xpbootcamp.primaryLockerRobot;
 
 import cn.xpbootcamp.locker.domain.Bag;
+import cn.xpbootcamp.locker.domain.Locker;
 import cn.xpbootcamp.locker.domain.Ticket;
 
+import java.util.List;
+
 public class PrimaryLockerRobot {
+    private List<Locker> managedLockers;
     public Ticket store(Bag bagA) {
-        return new Ticket();
+        return this.managedLockers.get(0).store(bagA);
+    }
+
+    public void setManagedLockers(List<Locker> lockers) {
+        this.managedLockers = lockers;
     }
 }
--- tdd201/src/test/java/cn/xpbootcamp/primaryLockerRobot/primaryLockerRobotTest.java
+++ tdd201/src/test/java/cn/xpbootcamp/primaryLockerRobot/primaryLockerRobotTest.java
@@ -3,11 +3,20 @@ package cn.xpbootcamp.primaryLockerRobot;
 import cn.xpbootcamp.locker.domain.Bag;
 import cn.xpbootcamp.locker.domain.Locker;
 import cn.xpbootcamp.locker.domain.Ticket;
+import cn.xpbootcamp.locker.exception.NoAvailableSpaceException;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class primaryLockerRobotTest {
@@ -15,13 +24,33 @@ public class primaryLockerRobotTest {
     public ExpectedException expectedEx = ExpectedException.none();
 
     @Test
-    public void given_PrimaryLockerRobot_manages_LockerA_and_lockerA_has_one_available_space_when_PrimaryLockerRobot_stores_bagA_then_store_successfully_and_get_valid_ticketA(){
+    public void given_PrimaryLockerRobot_manages_LockerA_and_lockerA_has_one_available_space_when_PrimaryLockerRobot_stores_bagA_then_store_successfully_and_get_valid_ticketA() {
         Locker lockerA = new Locker();
         lockerA.setAvailableSpaceNumber(1);
         Bag bagA = new Bag();
 
         PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot();
+        List<Locker> lockers = new ArrayList<Locker>();
+        lockers.add(lockerA);
+        primaryLockerRobot.setManagedLockers(lockers);
         Ticket ticketA = primaryLockerRobot.store(bagA);
         assertNotNull(ticketA);
     }
+
+    @Test
+    public void given_primaryLockerRobot_manages_LockerA_and_lockerA_has_no_available_space_when_PrimaryLockerRobot_stores_bagA_then_get_error_message() {
+        Locker lockerA = new Locker();
+        lockerA.setAvailableSpaceNumber(0);
+        Bag bagA = new Bag();
+
+        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot();
+        List<Locker> lockers = new ArrayList<Locker>();
+        lockers.add(lockerA);
+        primaryLockerRobot.setManagedLockers(lockers);
+
+        expectedEx.expect(NoAvailableSpaceException.class);
+        expectedEx.expectMessage("No available space");
+        primaryLockerRobot.store(bagA);
+    }
+
 }
