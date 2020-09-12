e09d4eecf37bdab9eacb3992672b3e3e8288a8ef
Ruihua Zhao <ruihua.zhao@myob.com>
2020-09-12 21:44:54 +0800

    add test case given_LockerRobotDirector_managed_1_manager_managed_2_locker_when_generate_report_then_generate_correct_report

--- tdd201/src/test/java/cn/xpbootcamp/lockerrobotdirector/LockerRobotDirectorTest.java
+++ tdd201/src/test/java/cn/xpbootcamp/lockerrobotdirector/LockerRobotDirectorTest.java
@@ -20,4 +20,17 @@ public class LockerRobotDirectorTest {
 
         assertEquals("M 1 2\n  L 1 2" , output);
     }
+
+    @Test
+    public void given_LockerRobotDirector_managed_1_manager_managed_2_locker_when_generate_report_then_generate_correct_report() {
+        Locker lockerA = new Locker(2);
+        Locker lockerB = new Locker(2);
+        lockerA.store(new Bag());
+        lockerA.store(new Bag());
+        LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(lockerA,lockerB), null);
+        LockerRobotDirector lockerRobotDirector = new LockerRobotDirector(Arrays.asList(lockerRobotManager));
+        String output = lockerRobotDirector.generateReport();
+
+        assertEquals("M 2 4\n  L 0 2\n  L 2 2" , output);
+    }
 }
